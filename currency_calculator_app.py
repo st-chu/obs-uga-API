e7c4fd4c4ba3150827c954215625d57d5682c8a2
Marek Starzyk <stachu.ms9@gmail.com>
2020-12-06 00:03:37 +0100

    add html and css

--- currency_calculator_app.py
+++ currency_calculator_app.py
@@ -1,15 +1,86 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import requests
 import csv
 
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
+date = data[0]['effectiveDate']
 exchange = data[0]['rates']
+
 with open('exchange_rate.csv', 'w') as exchange_rate_csv:
     fieldnames = ['currency', 'code', 'bid', 'ask']
     csv_writer = csv.DictWriter(exchange_rate_csv, fieldnames=fieldnames, delimiter=';')
     csv_writer.writeheader()
     for item in exchange:
         csv_writer.writerow(item)
-print(exchange)
+
+for item in exchange:
+    item['ask'] = round(item['ask'], 4)
+    item['bid'] = round(item['bid'], 4)
+
+
+def multiplier(items, operation, code):
+    for _item in items:
+        print(_item)
+        if _item['code'] == code:
+            mult = _item[operation]
+            return float(mult)
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'GET':
+        return render_template('exchange.html', items=exchange)
+    elif request.method == 'POST':
+        try:
+            form_data = request.form
+            code = form_data.get('code')
+            operation = form_data.get('operation')
+            amount = form_data.get('amount')
+            if bool(amount) is True:
+                try:
+                    amount = amount.replace(',', '.')
+                    amount = float(amount)
+                except ValueError:
+                    operation = 'notNumber'
+                    return render_template(
+                        'exchange.html',
+                        items=exchange,
+                        operation=operation,
+                        amount=amount
+                    )
+            if operation == 'bid':
+                bid = multiplier(exchange, operation, code)
+                score = round(amount / bid, 2)
+                amount_str = '{:.2f}'.format(amount)
+                return render_template(
+                    'exchange.html',
+                    items=exchange,
+                    operation=operation,
+                    amount=amount_str,
+                    score=score,
+                    code=code,
+                    date=date,
+                    bid=round(float(bid), 2)
+                )
+            elif operation == 'ask':
+                ask = multiplier(exchange, operation, code)
+                score = round(amount * ask, 2)
+                amount_str = '{:.2f}'.format(amount)
+                return render_template(
+                    'exchange.html',
+                    items=exchange,
+                    operation=operation,
+                    amount=amount_str,
+                    score=score,
+                    code=code,
+                    date=date,
+                    ask=round(float(ask), 2)
+                )
+        except TypeError:
+            operation = 'noValue'
+            return render_template('exchange.html', items=exchange, operation=operation)
